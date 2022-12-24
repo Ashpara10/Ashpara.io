cb31df9a2d500d4ebe438ecd6a0b4409297c2c62
Ashwin <ashwinparande1156610c@gmail.com>
2022-12-24 16:35:03 +0530

    responsive nav added

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar: React.FC = ({ children }) => {
       </div>
       <div className="w-full flex md:hidden items-center justify-between p-3  absolute top-0 right-0 left-0 ">
         <button
-          onClick={() => setNav(true)}
+          onClick={() => setNav(!nav)}
           className=" font-extrabold rounded-lg text-xl text-gray-900 dark:text-gray-100 p-3"
         >
           <MenuIcon />
@@ -43,12 +43,35 @@ const Navbar: React.FC = ({ children }) => {
         </button>
       </div>
       {nav && (
-        <div
-          onClick={() => setNav(false)}
-          className="w-full z-auto h-full fixed top-0 left-0 bottom-0 right-0 bg-black/50"
-        />
+        <div className="">
+          <ul className="w-full py-3 flex flex-col items-center justify-center gap-y-2">
+            <li
+              onClick={() => router.push("/")}
+              className="w-full flex items-center gap-r-2 justify-start hover:transition-all hover:bg-gray-100 text-left py-1.5 px-4 dark:hover:bg-[#212121] rounded-md"
+            >
+              Home
+            </li>
+            <li
+              onClick={() => router.push("/")}
+              className="w-full hover:transition-all hover:bg-gray-100 text-left py-1.5 px-4 dark:hover:bg-[#212121] rounded-md"
+            >
+              About
+            </li>
+            <li
+              onClick={() => router.push("/blog")}
+              className="w-full hover:transition-all hover:bg-gray-100 text-left py-1.5 px-4 dark:hover:bg-[#212121] rounded-md"
+            >
+              Blog
+            </li>
+            <li
+              onClick={() => router.push("/snippets")}
+              className="w-full hover:transition-all hover:bg-gray-100 text-left py-1.5 px-4 dark:hover:bg-[#212121] rounded-md"
+            >
+              Snippets
+            </li>
+          </ul>
+        </div>
       )}
-
       {/* <div
         className={`px-5 py-2 z-20 font-normal text-base w-64 translate-x-[-100%] overflow-hidden flex flex-col fixed top-0 left-0 bottom-0 bg-white/70 backdrop-blur-md border-r border-gray-300 dark:border-bdark dark:bg-dark/70 ${
           rendered
