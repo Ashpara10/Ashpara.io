983ce48e53759c7fe60c8fafcb5fa1523fa4d59a
Ashwin <ashwinparande1156610c@gmail.com>
2023-07-03 15:01:42 +0530

    supabase deleted and few new changes

--- Components/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const SUPABASE_KEY = String(process.env.SUPABASE_KEY);
-const SUPABASE_URL = "https://dfpdxhkdkjjmumvsxyzd.supabase.co";
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-export default supabase;
--- pages/api/blog/view/[slug].ts
+++ pages/api/blog/view/[slug].ts
@@ -1,20 +1,11 @@
 import type { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-import supabase from "../../../../Components/lib/supabase";
 
 const handler: NextApiHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
   const slugparam = req.query.slug.toString();
-
-  if (req.method === "GET") {
-    const { data } = await supabase.from("metrics").select(``);
-    res.status(200).json({ total: data });
-  }
-
-  if (req.method === "POST") {
-    // res.status(200).json({ total: view });
-  }
+  return res.status(200).json({ slug: slugparam });
 };
 
 export default handler;
